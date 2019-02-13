00122127b2ec80bd3759b82e1212ca97eb928b22
PhilosophicalMeat <1christieeth@hdsb.ca>
2019-02-13 15:07:56 -0500

    Started methods stuff

--- ICS4U1/src/SimpleMethods.java
+++ ICS4U1/src/SimpleMethods.java
@@ -16,27 +16,42 @@ public class SimpleMethods {
         outln(6);
         outln(.667);
         outln("there areeeee noooooo striiinnngggsss ooonn mmeeeee");
-
+        //Commands for testPrimeNumber
+        testPrimeNumber(12);
+        testPrimeNumber(17);
+        testPrimeNumber(6);
     }
+
     //out methods
-    public static void out (int iNumberToPrint){
+    public static void out(int iNumberToPrint) {
         System.out.print(iNumberToPrint); //prints int
     }
-    public static void out (double dNumberToPrint){
+
+    public static void out(double dNumberToPrint) {
         System.out.print(dNumberToPrint); //prints double
     }
-    public static void out (String stringToPrint){
+
+    public static void out(String stringToPrint) {
         System.out.print(stringToPrint); //prints string
     }
 
     //outln methods
-    public static void outln(int iNumToPrint){
+    public static void outln(int iNumToPrint) {
         System.out.println(iNumToPrint); //prints int on new line
     }
-    public static void outln(double dNumToPrint){
+
+    public static void outln(double dNumToPrint) {
         System.out.println(dNumToPrint); //prints double on new line
     }
-    public static void outln(String sLineToPrint){
+
+    public static void outln(String sLineToPrint) {
         System.out.println(sLineToPrint); //prints string on new line
+    }
+    //tests for prime number
+    public static void testPrimeNumber(int iTestPrime){
+        int pTestOne = iTestPrime % 2;
+        int pTestTwo = iTestPrime % 3;
+        String pResult = (pTestOne != 0 && pTestTwo != 0 && iTestPrime > 3) ? iTestPrime+" is a prime number" : iTestPrime+" is NOT a prime number";
+        System.out.println(pResult);
     }
 }
