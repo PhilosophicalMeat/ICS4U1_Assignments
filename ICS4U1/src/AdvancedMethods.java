1080c71f54d2acde6cb5aab65611c8e458fb98ab
PhilosophicalMeat <1christieeth@hdsb.ca>
2019-02-16 22:17:01 -0500

    Started methods stuff

---
+++ ICS4U1/src/AdvancedMethods.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.lang.String;
+public class AdvancedMethods {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int age;
+        System.out.println("Enter your age in years");
+        age = input.nextInt();
+
+    }
+    //age range int method
+    public static int inRange(int iAge, int max, int min){
+
+    }
+
+    //age range double method
+    public static double inRange(int dAge, int max, int min){
+
+    }
+}
