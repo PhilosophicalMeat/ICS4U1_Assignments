136135750132e54076805b34722a7b0f695029fb
PhilosophicalMeat <1christieeth@hdsb.ca>
2019-02-18 16:22:47 -0500

    Started methods stuff

--- ICS4U1/src/AdvancedMethods.java
+++ ICS4U1/src/AdvancedMethods.java
@@ -3,18 +3,25 @@ import java.lang.String;
 public class AdvancedMethods {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int age;
+        int iAge;
+
         System.out.println("Enter your age in years");
-        age = input.nextInt();
+        iAge = input.nextInt();
+
+        //age range integer method call
+        double dMoviePrice = (inRange(iAge, 14,64)) ? 10.50 : 7.99;
+        System.out.println("Price: $" +dMoviePrice);
 
     }
     //age range int method
-    public static int inRange(int iAge, int max, int min){
-
+    public static boolean inRange(int iValue, int min, int max){
+        return (iValue>min && iValue<max);
     }
-
     //age range double method
-    public static double inRange(int dAge, int max, int min){
-
+    public static boolean inRange(double dValue, int min, int max){
+        return(dValue>min && dValue<max);
     }
+
+
+
 }
