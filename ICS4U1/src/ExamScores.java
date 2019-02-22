72395ef06bc8c48b1a043799a0ec47829896a810
PhilosophicalMeat <1christieeth@hdsb.ca>
2019-02-22 11:24:01 -0500

    finished "advanced methods"

---
+++ ICS4U1/src/ExamScores.java
@@ -0,0 +1,10 @@
+import java.lang.String;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class ExamScores {
+    public static void main(String[] args) {
+
+    }
+}
