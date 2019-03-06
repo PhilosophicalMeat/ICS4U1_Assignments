c0943685b45cce873a3b2b8cf0ec101162676c17
PhilosophicalMeat <1christieeth@hdsb.ca>
2019-03-06 10:03:49 -0500

    finished "advanced methods"

--- ICS4U1/src/InterestAssignment.java
+++ ICS4U1/src/InterestAssignment.java
@@ -3,20 +3,24 @@ import java.io.File;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.*;
 
-public class InterestAssignment {
-    public static void main(String[] args) {
-        String fileAddress = "Desktop\\Project01Data(1)";
+public class InterestAssignment{
+    public static void main(String[] args) throws IOException{
+        String fileAddress = "C:\\Users\\Ethan\\Desktop\\Project01Data(1)";
         String[] dataArray = new String[5];
-
+        //Creating file
+        File dataFile = openFile(fileAddress);
+        //storing file in String array (row-by-row)
+        fillArray(dataFile,dataArray);
     }
     public static File openFile(String address){
         File tempFile = new File(address);
         return tempFile;
-
     }
-    public static void fillArray(Scanner fileCheck, String[] array){
+    public static void fillArray(File tempFile, String[] array) throws IOException{
         int i = 0;
+        Scanner fileCheck = new Scanner(tempFile);
         while(fileCheck.hasNext()){
             array[i] = fileCheck.nextLine();
             i++;
