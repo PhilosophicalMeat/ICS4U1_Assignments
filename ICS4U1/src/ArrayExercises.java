4173f886f07990e38ef073da7ee0f9fa6fb84f8e
PhilosophicalMeat <1christieeth@hdsb.ca>
2019-03-27 10:22:01 -0400

    "finished interest assignment"

--- ICS4U1/src/ArrayExercises.java
+++ ICS4U1/src/ArrayExercises.java
@@ -4,6 +4,12 @@ public class ArrayExercises {
         int[] integerArray = {1, 4, 6, 7, 9, 2};
         smallestDifference(integerArray);
 
+        //Exercise 2
+        double[][] exTwoArray = new double[10][10];
+        fill2DArray(exTwoArray);
+        maxRowSum(exTwoArray);
+
+
     }
     public static void smallestDifference(int[] intArray){
         //Find smallest value in array
@@ -27,5 +33,31 @@ public class ArrayExercises {
         //finding the distance between the smallest and second smallest values
         int difference = minTwo - min;
         System.out.println("Distance between the values: " +difference);
+    }
+    public static void fill2DArray(double[][]dArray){
+        for (int i = 0; i < dArray.length; i++) {
+            for (int j = 0; j < dArray[i].length; j++) {
+                dArray[i][j] = Math.random();
+            }
+        }
+    }
+    public static void maxRowSum(double[][] dArray){
+        double[] rowSums = new double[dArray.length];
+        for (int i = 0; i < dArray.length; i++) {
+            for (int j = 0; j < dArray[i].length; j++) {
+                rowSums[i] += dArray[i][j];
+            }
+        }
+        double max = 0;
+        int maxLoc = 0;
+        for (int i = 0; i < rowSums.length; i++) {
+            for (int j = 0; j < rowSums.length; j++) {
+                max = (rowSums[j] > rowSums[i]) ? rowSums[j] : max;
+                maxLoc = (rowSums[j] > rowSums[i]) ? j : maxLoc;
+            }
+        }
+        double roundedVal = Math.round(max*(Math.pow(10,2))); //multplies by desired # of decimals
+        double resultVal = roundedVal/(Math.pow(10,2));
+        System.out.println("Row with max sum: " +maxLoc+ "\n" + "Sum: " +resultVal);
     }
 }
