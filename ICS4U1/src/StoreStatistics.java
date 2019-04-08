e77682db16eea8ec5e01193a7194d2f6b21b5846
PhilosophicalMeat <1christieeth@hdsb.ca>
2019-04-08 10:05:09 -0400

    "finished interest assignment"

--- ICS4U1/src/StoreStatistics.java
+++ ICS4U1/src/StoreStatistics.java
@@ -1,7 +1,34 @@
 import java.io.*;
 import java.util.Scanner;
 public class StoreStatistics {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException{
+        File storeData = new File("C:\\Users\\Ethan\\Desktop\\Project02Data (1).txt");
+        Scanner fileChecker = new Scanner(storeData);
+        String[][] sDataArraySorted = new String[17113][4];
+        //Store each line of data into a 2D array
+        fillString2DArray(fileChecker, sDataArraySorted);
+        //Create an array to identify store locations based on their three-digit ID
+        String[][] storeLoc = {
+                {"101", "St. John's, Newfoundland"},
+                {"102", "Halifax, Nova Scotia"},
+                {"103", "Fredericton, New Brunswick"},
+                {"201", "Quebec City, Quebec"},
+                {"202", "Montreal, Quebec"},
+                {"301", "Ottawa, Ontario"},
+                {"302", "Toronto, Ontario"},
+                {"303" ,"Mississauga, Ontario"},
+                {"304", "Hamilton, Ontario"},
+                {"305", "Kitchener, Ontario"},
+                {"401", "Winnipeg, Manitoba"}
+
+
+        };
     }
+
+    public static void fillString2DArray(Scanner fileChecker, String[][] sDataArray){
+        for (int i = 0; fileChecker.hasNext(); i++) {
+            sDataArray[i] = fileChecker.nextLine().split(",");
+        }
+    }
+
 }
