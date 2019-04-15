6a37c38fd6acc1db091d1eb6ba5298e09aa59b97
PhilosophicalMeat <1christieeth@hdsb.ca>
2019-04-15 12:38:53 -0400

    "completed the store statistics assignment. tested, works 100%"

---
+++ ICS4U1/src/CalendarDates.java
@@ -0,0 +1,8 @@
+import java.util.Calendar;
+import java.util.Scanner;
+import java.io.*;
+public class CalendarDates{
+    public static void main(String[] args) throws IOException{
+        
+    }
+}
