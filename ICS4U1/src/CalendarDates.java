2d524fa383c21c5b41d4b237edd8912b23ca2e73
PhilosophicalMeat <1christieeth@hdsb.ca>
2019-04-15 23:05:06 -0400

    "completed the store statistics assignment. tested, works 100%"

--- ICS4U1/src/CalendarDates.java
+++ ICS4U1/src/CalendarDates.java
@@ -1,8 +1,30 @@
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 import java.io.*;
 public class CalendarDates{
     public static void main(String[] args) throws IOException{
-        
+        //creating scanner to collect inputted data
+        Scanner input = new Scanner(System.in);
+        int[] dateArray = new int[3], timeArray = new int[2]; //made it double just to be safe. might make it integer in the future
+        String temp = "";
+        String[] tempArrayDate= new String[3];
+        String[] tempArrayTime = new String[2];
+        //prompt for the date
+        System.out.println("Enter the current date (year/month/day)");
+        temp = input.nextLine();
+        tempArrayDate = temp.split("/");
+        dateArray[0] = Integer.parseInt(tempArrayDate[0]);
+        dateArray[1] = Integer.parseInt(tempArrayDate[1]);
+        dateArray[2] = Integer.parseInt(tempArrayDate[2]);
+
+        //prompt for the time of day
+        System.out.println("Enter the time of day (hour:minute)");
+        temp = input.nextLine();
+        tempArrayTime = temp.split(":");
+        timeArray[0] = Integer.parseInt(tempArrayTime[0]);
+        timeArray[1] = Integer.parseInt(tempArrayTime[1]);
+        //testing the calendar class
+
     }
 }
---
+++ ICS4U1/src/FirstSwing.java
@@ -0,0 +1,12 @@
+import java.awt.event.*;
+import javax.swing.*;
+public class FirstSwing {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+
+        frame.setSize(400,500);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
