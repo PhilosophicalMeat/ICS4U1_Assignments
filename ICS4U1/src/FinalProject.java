7640b2940257564658d6eaba8d8fef3f478be889
PhilosophicalMeat <1christieeth@hdsb.ca>
2019-06-06 10:54:05 -0400

    "Finished ArrayListsAssignment03. Moving on to Final Project for the time being... fuck."

---
+++ ICS4U1/src/FinalProject.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import javax.swing.text.NumberFormatter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.text.NumberFormat;
+
+public class FinalProject {
+    public static void main(String[] args) throws IOException{
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    }
+}
